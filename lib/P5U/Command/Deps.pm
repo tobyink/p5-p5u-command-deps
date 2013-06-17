e987da5cfe511d578f6a4f9024141bfdb7ddac86
Toby Inkster <mail@tobyinkster.co.uk>
2013-06-17 22:47:07 +0100

    bump version; changelog

--- lib/P5U/Command/Deps.pm
+++ lib/P5U/Command/Deps.pm
@@ -7,7 +7,7 @@ use P5U-command;
 
 BEGIN {
 	$P5U::Command::Deps::AUTHORITY = 'cpan:TOBYINK';
-	$P5U::Command::Deps::VERSION   = '0.004';
+	$P5U::Command::Deps::VERSION   = '0.005';
 }
 
 use constant abstract      => q  <scan Perl source code for dependencies>;
